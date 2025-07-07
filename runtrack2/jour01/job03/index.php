2a2c9b1f75270380fb97d8ab8fcc36e46c88e30d
adrien-saccaggi <adrien.saccaggi@laplateforme.io>
2025-07-07 16:50:52 +0200

    Add files via upload

---
+++ runtrack2/jour01/job03/index.php
@@ -0,0 +1,34 @@
+<?php
+$mybool = true;
+$val = 1234;
+echo gettype($mybool);
+echo $mybool ." ". $val;
+
+echo "
+<table>
+ <thead>
+    <tr>
+      <th>type</th>
+      <th>nom</th>
+      <th>valeur</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>boolean</td>
+      <td>mybool</td>
+      <td>true</td>
+    </tr>
+    <tr>
+      <td>type</td>
+      <td>gettype</td>
+      <td>mybool</td>
+    </tr>
+    <tr>
+      <td>entier</td>
+      <td>val</td>
+      <td>1234</td>
+    </tr>
+  </tbody>
+</table>"
+?>
