eb20be3d4044be5cfeb2be3a65fc3dbeecf69baa
adrien-saccaggi <adrien.saccaggi@laplateforme.io>
2025-07-07 13:54:32 +0200

    index.php

--- runtrack2/jour01/job01/index.php
+++ runtrack2/jour01/job01/index.php
@@ -3,5 +3,6 @@ $str1 = 'LaPlateforme';
 $str2 = 'Vive';
 $str3 = '!';
 $val = 10;
+$myBool = false; //quand il est en true il rajoute un 1//
 
-echo $str2 . " " . $str1 . " " .  $str3. " " . $val ?>
+echo $str2 . " " . $str1 . " " .  $str3. " " . $val. $myBool ?>
