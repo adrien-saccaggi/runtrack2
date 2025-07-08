801f2d5b759e1699f3106693a3475703eba1f78c
adrien-saccaggi <adrien.saccaggi@laplateforme.io>
2025-07-08 11:54:51 +0200

    index.php

--- runtrack2/jour01/job03/index.php
+++ runtrack2/jour01/job03/index.php
@@ -1,34 +1,53 @@
 <?php
-$mybool = true;
-$val = 1234;
-echo gettype($mybool);
-echo $mybool ." ". $val;
+$booleanVar = true; 
+$intVar = 50;
+$stringVar = "Texte";
+$floatVar = 5.30;
 
-echo "
-<table>
- <thead>
-    <tr>
-      <th>type</th>
-      <th>nom</th>
-      <th>valeur</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>boolean</td>
-      <td>mybool</td>
-      <td>true</td>
-    </tr>
-    <tr>
-      <td>type</td>
-      <td>gettype</td>
-      <td>mybool</td>
-    </tr>
-    <tr>
-      <td>entier</td>
-      <td>val</td>
-      <td>1234</td>
-    </tr>
-  </tbody>
-</table>"
+$fonctionArray = [
+    [1, 2],
+    [3, 4],
+];
+
+$variables = [
+    'booleanVar' => $booleanVar,
+    'intVar' => $intVar,
+    'stringVar' => $stringVar, 
+    'floatVar'  => $floatVar
+
+];
+
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="main.css">
+    </head>
+<body>
+
+<table>
+    <thead>
+        <tr>
+            <th> Type des variables</th>
+            <th> Nom des variables </th>
+            <th> Valeurs </th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($variables as $name => $value): ?>
+            <tr>
+            <td><?php echo gettype($value); ?> </td>
+            <td><?php echo $name; ?></td>
+            <td> 
+            <?php if (is_bool($value)) {
+                echo $value ? 'true' : 'false'; 
+            }
+                else { 
+                    echo htmlspecialchars((string) $value); 
+                }
+            ?>
+            </td>
+            </tr> 
+        <?php endforeach; ?>
